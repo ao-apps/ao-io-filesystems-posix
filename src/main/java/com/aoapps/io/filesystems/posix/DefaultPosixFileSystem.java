f36a413365053ce3c6e3c1eac654b4e5224febb2
AO Industries, Inc <support@aoindustries.com>
2024-10-12 01:09:41 -0500

    checkstyle-10.18.2 compatibility

--- src/main/java/com/aoapps/io/filesystems/posix/DefaultPosixFileSystem.java
+++ src/main/java/com/aoapps/io/filesystems/posix/DefaultPosixFileSystem.java
@@ -1,6 +1,6 @@
 /*
  * ao-io-filesystems-posix - POSIX filesystem abstraction.
- * Copyright (C) 2015, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2015, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -49,6 +49,9 @@ public class DefaultPosixFileSystem extends JavaFileSystem implements PosixFileS
     return instance;
   }
 
+  /**
+   * Creates a new {@link DefaultPosixFileSystem}.
+   */
   protected DefaultPosixFileSystem() {
     super(FileSystems.getDefault());
     if (!isSingleRoot) {
--- src/main/java/com/aoapps/io/filesystems/posix/ReadOnlyPosixFileSystem.java
+++ src/main/java/com/aoapps/io/filesystems/posix/ReadOnlyPosixFileSystem.java
@@ -1,6 +1,6 @@
 /*
  * ao-io-filesystems-posix - POSIX filesystem abstraction.
- * Copyright (C) 2015, 2021, 2022  AO Industries, Inc.
+ * Copyright (C) 2015, 2021, 2022, 2024  AO Industries, Inc.
  *     support@aoindustries.com
  *     7262 Bull Pen Cir
  *     Mobile, AL 36695
@@ -38,6 +38,9 @@ public class ReadOnlyPosixFileSystem extends ReadOnlyFileSystem implements Posix
 
   private final PosixFileSystem wrapped;
 
+  /**
+   * Creates a new {@link ReadOnlyPosixFileSystem}.
+   */
   public ReadOnlyPosixFileSystem(PosixFileSystem wrapped) {
     super(wrapped);
     this.wrapped = wrapped;
